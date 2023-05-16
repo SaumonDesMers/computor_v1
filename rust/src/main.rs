66d0959c8cb6a98c13e44a2810ad6848d35b9f1f
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-05-16 12:59:23 +0200

    rust: read program args

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+	
+	// let eq = match args.get(1) {
+	// 	Some(eq) => eq,
+	// 	None => {
+	// 		println!("Missing an argument !");
+	// 		return;
+	// 	}
+	// };
+
+	if args.len() < 2 {
+		println!("Missing an argument !");
+		return;
+	}
+
+	let mut eq = String::from(&args[1]);
+
+	eq.retain(|c| c != ' ');
+
+	dbg!(eq);
 }
